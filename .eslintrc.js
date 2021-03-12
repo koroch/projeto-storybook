c01f30284f9f0d7122354a13dff8f9ca469fbb90
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-03-12 00:18:43 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        'indent': ['error', 4],
+        'comma-spacing': ['error', {before: false, after: true}]
+    },
+    'parser': 'babel-eslint',
+};
